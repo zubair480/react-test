a621d4bdb22b2787d92f980d9383cb324a062fd0
zubair480 <zubairzafar480@gmail.com>
2022-08-01 12:30:01 +0500

    first commit

---
+++ src/components/Movieslist.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Movieslist() {
+  return (
+    <section>
+      <ul className="styled w-100 pl-0" data-testid="moviesList"></ul>
+    </section>
+  )
+}
+
+export default Movieslist
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react"
+
+function Search() {
+  return (
+    <section className="layout-row justify-content-center mb-40">
+      <input type="text" placeholder="Search for movie by name" className="w-75 py-2" data-testid="search" />
+    </section>
+  )
+}
+
+export default Search
